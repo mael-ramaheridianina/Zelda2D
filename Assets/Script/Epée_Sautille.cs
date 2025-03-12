90a834cff85691dca143ed9edebe3c5c480d0774
Malou <votre.email@example.com>
2025-03-12 16:42:40 +0100

    Ajout de la classe Epée_Sautille avec un effet de rebond sinusoïdal

--- Assets/Script/Epée_Sautille.cs
+++ Assets/Script/Epée_Sautille.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class Epée_Sautille : MonoBehaviour
 {
+    [Header("Animation Settings")]
+    [SerializeField] private float bounceHeight = 0.2f;
+    [SerializeField] private float bounceSpeed = 2f;
     
+    private Vector3 startPosition;
+    private float timeOffset;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        // Offset aléatoire pour que toutes les épées ne sautent pas en même temps
+        timeOffset = Random.Range(0f, 2f * Mathf.PI);
+    }
+
+    void Update()
+    {
+        // Calcule la position Y avec un effet de rebond sinusoïdal
+        float newY = startPosition.y + Mathf.Abs(Mathf.Sin((Time.time + timeOffset) * bounceSpeed)) * bounceHeight;
+        
+        // Applique la nouvelle position
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
 }
