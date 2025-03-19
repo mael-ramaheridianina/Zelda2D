c5f5cb975e7cafecb08d0a888271ae21aed5e178
Malou <votre.email@example.com>
2025-03-19 16:35:02 +0100

    Désactivation de la Foudre quand la scène est lancé ajout d'une nouvelle image de viseur

--- Assets/Script/FoudreScript.cs
+++ Assets/Script/FoudreScript.cs
@@ -5,7 +5,7 @@ public class FoudreScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+          gameObject.SetActive(false);
     }
 
     // Update is called once per frame
