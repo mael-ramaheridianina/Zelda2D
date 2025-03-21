b84fea4b5b01060ead7165a044e1170f06aee708
Malou <votre.email@example.com>
2025-03-21 19:54:29 +0100

    Mise à jour du script CameraFollow pour ajouter le suivi manuel et la gestion des positions valides. Ajout de nouveaux sprites pour le viseur avec des configurations mises à jour.

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -2,15 +2,63 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private float cameraSpeed = 5f; // Vitesse de déplacement manuel
+    [SerializeField] private Vector3 offset = new Vector3(0, 0, -10);
+    private bool isFollowing = true;
+    private Vector3 lastValidPosition;
+    private bool hasValidPosition = false;
+
     void Start()
     {
-        
+        if (target != null)
+        {
+            lastValidPosition = target.position;
+            hasValidPosition = true;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!isFollowing)
+        {
+            // Déplacement manuel avec les flèches
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            
+            Vector3 movement = new Vector3(horizontalInput, verticalInput, 0);
+            transform.position += movement * cameraSpeed * Time.deltaTime;
+        }
+        else if (target != null)
+        {
+            // Suivi normal du player
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+            lastValidPosition = transform.position - offset;
+            hasValidPosition = true;
+        }
+        // Sinon on reste à la dernière position valide
+        else if (hasValidPosition)
+        {
+            transform.position = lastValidPosition + offset;
+        }
+    }
+
+    public void StopFollowing()
+    {
+        isFollowing = false;
+        // Sauvegarde la position actuelle comme dernière position valide
+        if (target != null)
+        {
+            lastValidPosition = target.position;
+            hasValidPosition = true;
+        }
+    }
+
+    public void StartFollowing()
+    {
+        isFollowing = true;
     }
 }
