4d4ad708718809ce1c492d5f53cd2f62f9d529bf
Malou <votre.email@example.com>
2025-03-28 19:22:31 +0100

    ajout du déplacement au dessu du player si touché

--- Assets/Script/StaminaVesselScript.cs
+++ Assets/Script/StaminaVesselScript.cs
@@ -1,30 +1,57 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class StaminaVesselScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Animation Settings")]
+    [SerializeField] private float floatHeight = 1.5f;
+    [SerializeField] private float moveUpDuration = 0.5f;
+    [SerializeField] private float visibleDuration = 2.5f;
+    [SerializeField] private float fadeOutDuration = 0.3f;
 
-    // Update is called once per frame
-    void Update()
+    private SpriteRenderer spriteRenderer;
+    private bool isCollected = false;
+
+    private void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (!isCollected && other.CompareTag("Player"))
         {
             PlayerScript player = other.GetComponent<PlayerScript>();
             if (player != null)
             {
+                isCollected = true;
                 player.StartCelebration();
-                // Optionally destroy the stamina vessel after collection
-                Destroy(gameObject);
+                StartPickupSequence(other.transform.position);
             }
         }
+    }
+
+    private void StartPickupSequence(Vector3 playerPosition)
+    {
+        GetComponent<Collider2D>().enabled = false;
+
+        // Position cible juste au-dessus du player
+        Vector3 targetPosition = new Vector3(
+            playerPosition.x,
+            playerPosition.y + floatHeight,
+            transform.position.z
+        );
+
+        // Déplacement vers la position au-dessus du player
+        transform.DOMove(targetPosition, moveUpDuration)
+            .SetEase(Ease.OutQuad)
+            .OnComplete(() => {
+                DOVirtual.DelayedCall(visibleDuration, () => {
+                    spriteRenderer.DOFade(0, fadeOutDuration)
+                        .OnComplete(() => {
+                            Destroy(gameObject);
+                        });
+                });
+            });
     }
 }
